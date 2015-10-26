18492cd4d5be8edaae398bbadf510c29b78e01fb
Barry Smith <barry.of.smith@gmail.com>
2015-10-26 23:55:13 +0000

    Added some lovely mountains

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,15 @@
 
 void loadAt(glm::ivec3 pos, LibVolume::Engine::VoxelTerrain& terrain)
 {
+	std::vector<glm::vec3> colours = {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.4f, 0.9f, 0.4f), glm::vec3(1.3f, 1.3f, 4.0f), glm::vec3(4.5f, 3.7f, 3.0f), glm::vec3(0.6f, 0.6f, 0.6f), glm::vec3(4.6f, 4.6f, 4.6f)};
+
 	if (terrain.existsAt(pos))
 		return;
 
 	LibVolume::Generation::PerlinNoise noise;
 
 	terrain.loadAt(pos);
-	auto region = terrain.getAt(pos);
+	LibVolume::Engine::VoxelTerrainChild* region = terrain.getAt(pos);
 
 	for (int x = 0; x < 32; x ++)
 	{
@@ -26,26 +28,39 @@ void loadAt(glm::ivec3 pos, LibVolume::Engine::VoxelTerrain& terrain)
 		{
 			for (int z = 0; z < 32; z ++)
 			{
-				glm::vec3 loc = glm::vec3(region->location) + glm::vec3(x, y, z);
-				loc += glm::vec3(0, 0, 60);
+				LibVolume::Data::Voxel* voxel = region->getAt(glm::ivec3(x, y, z));
+
+				glm::vec3 loc = (glm::vec3(region->location) + glm::vec3(x, y, z)) * 0.8f;
 				float density = 0.0f;
-				float scalar = 0.8;
 
-				glm::vec3 offset = noise.getPerlinVec3(glm::vec4(loc.x * scalar, loc.y * scalar, scalar * loc.z * 1.0, 7.0) * 4.0f, -7.5, 1.0, 1.0);
-				density = (-loc.z + 64.0f) / 20.0f + noise.getPerlin(glm::vec4(scalar * loc.x + offset.x * 20.0f, scalar * loc.y + offset.y * 20.0f, scalar * loc.z + offset.z * 20.0f, 2.0) * 3.0f, -7.5, 2.0, 1.0);
+				float var = (noise.getPerlin(glm::vec4(loc, 7.0), -6, 3.0, 2.6) + 1.0f) * 0.5f;
+				density = glm::max(0.0f, glm::min(1.0f, 16.0f * var / (loc.z - 16.0f)));
 
-				density += 0.02f * noise.getPerlin(glm::vec4(50.0f * loc.x, 50.0f * loc.y, 50.0f * loc.z, 6.0) * 3.0f, -7.5, 1.0, 1.0);
+				glm::vec2 caves;
+				caves.x = glm::abs(noise.getPerlin(glm::vec4(loc, 14.0f), -6, 1.0, 1.0));
+				caves.y = glm::abs(noise.getPerlin(glm::vec4(loc + glm::vec3(33.0f, 57.0f, 60.0f), 8.0f), -6, 1.0, 1.0));
+				if (caves.x < 0.06f && caves.y < 0.06f)
+					density *= glm::min(1600.0f * caves.x * caves.y, 1.0f);
 
-				float variant = noise.getPerlin(glm::vec4(loc, 1.0) * 1.0f, -7.0, 3.0, 1.0);
-				region->getAt(glm::ivec3(x, y, z))->density = 0.5 * (1.0 + variant);
-				region->getAt(glm::ivec3(x, y, z))->density = (density + 1.0f) / 0.5f;
+				voxel->density = density;
+
+				if (loc.z < 28.0f)
+					voxel->type = 2;
+				else if (loc.z < 30.0f)
+					voxel->type = 3;
+				else if (loc.z < 37.0f)
+					voxel->type = 1;
+				else if (loc.z < 39.0f)
+					voxel->type = 4;
+				else
+					voxel->type = 5;
 			}
 		}
 	}
 
-	region->mesh->colour = glm::vec3(0.5, 2.0, 0.0);
+	region->colour_table = colours;
 
-	region->extract(LibVolume::Engine::MeshingAlgorithm::MarchingCubes);
+	terrain.reMesh(pos);
 }
 
 int main(int argc, char* argv[])
@@ -56,35 +71,14 @@ int main(int argc, char* argv[])
 	LibVolume::Engine::Realm realm;
 	realm.linkTo(window);
 
-	realm.camera.state.position = glm::vec3(-20.0, 0.0, 0.0);
+	realm.background_colour = glm::vec3(4.0, 3.0, 8.0) * 0.2f;
 
-	LibVolume::Render::Structures::Light sun(LibVolume::Render::Structures::LightType::Directional, glm::vec3(0.25, 0.25, -1.0), glm::vec3(3.0, 3.0, 2.9), 0.05);
+	realm.camera.state.position = glm::vec3(-20.0, 0.0, 32.0);
+
+	LibVolume::Render::Structures::Light sun(LibVolume::Render::Structures::LightType::Directional, glm::vec3(0.8, 0.25, -1.0), glm::vec3(3.0, 3.0, 3.9) * 0.4f, 0.2);
 	realm.addLight(sun);
 
-	LibVolume::Engine::VoxelTerrain terrain(glm::ivec3(32, 32, 32));
-
-	for (int x = -1; x < 1; x ++)
-	{
-		for (int y = -1; y < 1; y ++)
-		{
-			for (int z = 0; z < 1; z ++)
-			{
-				loadAt(glm::ivec3(x, y, z), terrain);
-				//terrain.getAt(glm::ivec3(x, y, z))->extract(LibVolume::Engine::MeshingAlgorithm::MarchingCubes);
-			}
-		}
-	}
-
-	for (int x = -1; x < 1; x ++)
-	{
-		for (int y = -1; y < 1; y ++)
-		{
-			for (int z = 0; z < 1; z ++)
-			{
-				terrain.getAt(glm::ivec3(x, y, z))->extract(LibVolume::Engine::MeshingAlgorithm::MarchingCubes);
-			}
-		}
-	}
+	LibVolume::Engine::VoxelTerrain terrain(glm::ivec3(32, 32, 32), LibVolume::Engine::MeshingAlgorithm::MarchingCubes, true);
 
 	realm.addObject(terrain);
 
@@ -116,13 +110,13 @@ int main(int argc, char* argv[])
 		if (window.event_manager.keyboard_state.key_e)
 			realm.camera.state.orientation = glm::f64quat(glm::vec3(0.0, 0.0, 0.03)) * realm.camera.state.orientation;
 
-		for (int x = -1; x < 1; x ++)
+		for (int x = -2; x < 2; x ++)
 		{
-			for (int y = -1; y < 1; y ++)
+			for (int y = -2; y < 2; y ++)
 			{
-				for (int z = -1; z < 1; z ++)
+				for (int z = 0; z < 2; z ++)
 				{
-					loadAt(glm::ivec3(x, y, z) + glm::ivec3(realm.camera.state.position) / 32, terrain);
+					loadAt(glm::ivec3(x, y, z) + glm::ivec3(realm.camera.state.position.x, realm.camera.state.position.y, 0) / 32, terrain);
 					//terrain.getAt(glm::ivec3(x, y, z))->extract(LibVolume::Engine::MeshingAlgorithm::MarchingCubes);
 				}
 			}
